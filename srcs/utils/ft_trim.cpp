dad4e5f622fc3ad8c6257188eb8fcc845b40fed1
Adrien <chuadrien2003@gmail.com>
2025-10-29 01:44:32 +0100

    feat(utils): add ft_split function
    +ft_split
    ~ft_trim

--- srcs/utils/ft_trim.cpp
+++ srcs/utils/ft_trim.cpp
@@ -6,18 +6,18 @@
 /*   By: achu <achu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 01:05:25 by achu              #+#    #+#             */
-/*   Updated: 2025/10/29 01:10:44 by achu             ###   ########.fr       */
+/*   Updated: 2025/10/29 01:32:30 by achu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 
-std::string		ft_trim(std::string pStr, std::string pDelim)
+std::string		ft_trim(const std::string& pStr, const std::string& pDelims)
 {
-	size_t	start = pStr.find_first_not_of(pDelim);
+	size_t	start = pStr.find_first_not_of(pDelims);
 	if (start == std::string::npos)
 		return ("");
 
-	size_t	end = pStr.find_last_not_of(pDelim);
+	size_t	end = pStr.find_last_not_of(pDelims);
 	return (pStr.substr(start, end - start + 1));
 }
