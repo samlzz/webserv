c15cee0b6e982f0e7b44ca1b33ef70efeba01ffb
Adrien <chuadrien2003@gmail.com>
2025-11-03 02:57:34 +0100

    feat(http-response): add header & file

---
+++ srcs/http/HttpResponse.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpResponse.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achu <achu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 23:32:03 by achu              #+#    #+#             */
+/*   Updated: 2025/11/02 23:33:32 by achu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __HTTP_RESPONSE_H__
+#define __HTTP_RESPONSE_H__
+
+class HttpResponse {
+
+private:
+	
+public:
+	HttpResponse(void);
+	~HttpResponse(void);
+};
+
+#endif
