1bd77614cee6d25fa093e9975646f24c9d111871
um4ss <sliziard@icloud.com>
2025-12-10 02:28:12 +0100

    feat(config): add AST extraction for server and location blocks
    Implement AST-to-Config conversion:
    - parsing errors (WsParseError, PropError, ValueError)
    - generic fillDest helpers
    - extraction of server directives (host, port, methods, root, cgi, error_page...)
    - extraction of location blocks
    - sorting of locations by path length
    
    Add new files:
    - Srcs/parsing/configParse.hpp
    - Srcs/parsing/extractAst.cpp

---
+++ Srcs/parsing/configParse.hpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   configParse.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 09:10:18 by sliziard          #+#    #+#             */
+/*   Updated: 2025/12/10 02:05:48 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __PARSEPROPS_HPP__
+# define __PARSEPROPS_HPP__
+
+# include <stdexcept>
+# include <stdint.h>
+
+# include "ftpp/AstNode.hpp"
+# include "parsing/Config.hpp"
+
+namespace config_parse
+{
+
+// ============================================================================
+// Errors
+// ============================================================================
+class WsParseError : public std::runtime_error {
+public:
+	WsParseError(const std::string &msg): std::runtime_error(msg) {}
+};
+
+class PropError : public WsParseError {
+public:
+	PropError(const std::string &msg): WsParseError(msg) {}
+	PropError(const std::string &prop, const std::string &msg)
+		: WsParseError(prop + ": " + msg) {}
+};
+
+class ValueError : public PropError {
+public:
+	ValueError(const std::string &what)
+		: PropError("Empty " + what + " value") {}
+	ValueError(const std::string &what, const std::string &value)
+		: PropError("Invalid " + what + " value", "'" + value + "'") {}
+};
+
+// ============================================================================
+// Extraction
+// ============================================================================
+
+Config::Server	extractServer(const AstNode *serverNode);
+
+// O is T or Optionnal<T>
+template<typename T, typename O>
+void	fillDest(O &dest, const AstNode *node, const std::string &prop,
+					T (*extract)(const std::string &))
+{
+	if (!node->hasAttr(prop))
+		return;
+	dest = extract(node->getAttr(prop, ""));
+}
+
+template<typename O>
+void	fillDest(O &dest, const AstNode *node, const std::string &prop)
+{
+	if (!node->hasAttr(prop))
+		return;
+	dest = node->getAttr(prop, "");
+}
+
+} // namespace config_parse
+
+#endif /* __PARSEPROPS_HPP__ */
---
+++ Srcs/parsing/extractAst.cpp
@@ -0,0 +1,166 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extractAst.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 09:36:59 by sliziard          #+#    #+#             */
+/*   Updated: 2025/12/10 02:06:45 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <algorithm>
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "Http/HttpTypes.hpp"
+#include "ftpp/AstNode.hpp"
+#include "parsing/Config.hpp"
+#include "parsing/configParse.hpp"
+#include "parsing/parseUtils.hpp"
+
+namespace config_parse
+{
+
+// ========================================================================
+// Low-level Extraction Helpers
+// ========================================================================
+
+// both server and location
+static std::vector<http::e_method>	_extractMethods(const std::string &s)
+{
+	std::vector<http::e_method>	methods;
+	std::vector<std::string>	toks = splitWords(s);
+
+	size_t i = 0;
+	while (i < toks.size())
+		methods.push_back(parseMethod(toks[i++]));
+	return methods;
+}
+
+// server only
+static uint64_t	_extractPort(const std::string &s)
+{
+	return parseUnsigned(s, "port");
+}
+
+// location only
+static Config::StatusPath	_extractReturn(const std::string &s)
+{
+	Config::StatusPath			redirect;
+	std::vector<std::string>	toks = splitWords(s);
+
+	if (toks.size() > 2)
+		throw ValueError("redirect", s);
+	redirect.code = parseStatusCode(toks[0]);
+	redirect.path = toks[1];
+	return redirect;
+}
+
+// ========================================================================
+// Composite Directive Handlers (error_page, cgi)
+// ========================================================================
+
+static inline void	_appendErrorsPageItem(Config::t_errPages &errPages,
+											std::string &defaultPage,
+											const AstNode *errorPageNode)
+{
+	Config::assertProp(errorPageNode, "path", "error_page");
+	std::vector<std::string> toks = splitWords(
+		errorPageNode->getAttr("codes", "")
+	);
+
+	if (toks.empty())
+		return fillDest(defaultPage, errorPageNode, "path");
+
+	std::string	path = errorPageNode->getAttr("path", "");
+	for (size_t i = 0; i < toks.size(); ++i)
+		errPages[parseStatusCode(toks[i])] = path;
+}
+
+static inline void	_appendCgiItem(Config::t_dict &exts,
+									const AstNode *cgiNode)
+{
+	Config::assertProp(cgiNode, "extension", "cgi");
+	Config::assertProp(cgiNode, "path", "cgi");
+
+	exts[cgiNode->getAttr("extension", "")] = cgiNode->getAttr("path", "");
+}
+
+// ========================================================================
+// Location Block Extraction
+// ========================================================================
+
+Config::Server::Location	extractLocation(const AstNode *locationNode)
+{
+	Config::Server::Location	loc;
+
+	fillDest(loc.path, locationNode, "path");
+	fillDest(loc.methods, locationNode, "methods", _extractMethods);
+	fillDest(loc.root, locationNode, "root");
+	fillDest(loc.index, locationNode, "index");
+	fillDest(loc.autoindex, locationNode, "autoindex", parseBool);
+	fillDest(loc.uploadPath, locationNode, "upload");
+	fillDest(loc.redirect, locationNode, "redirect", _extractReturn);
+
+	const std::vector<AstNode *> &children = locationNode->children();
+	for (size_t i = 0; i < children.size(); ++i)
+	{
+		AstNode	*prop = children[i];
+
+		if (!prop)
+			continue;
+		if (prop->type() == "cgi")
+			_appendCgiItem(loc.cgiExts, prop);
+		else if (prop->type() == "error_page")
+		{
+			if (loc.defaultErrPage.isNone())
+				loc.defaultErrPage = "";
+			_appendErrorsPageItem(loc.errorPages, *loc.defaultErrPage, prop);
+		}
+	}
+	return loc;
+}
+
+// ========================================================================
+// Server Block Extraction
+// ========================================================================
+
+void	extractServDirectives(Config::Server &serv, const AstNode *serverNode)
+{
+	fillDest(serv.host, serverNode, "host");
+	fillDest(serv.port, serverNode, "port", _extractPort);
+	fillDest(serv.maxBodySize, serverNode, "client_max_body_size", parseSize);
+	fillDest(serv.d_methods, serverNode, "methods", _extractMethods);
+	fillDest(serv.d_root, serverNode, "root");
+	fillDest(serv.d_index, serverNode, "index");
+	fillDest(serv.d_autoindex, serverNode, "autoindex", parseBool);
+
+	const std::vector<AstNode *> &children = serverNode->children();
+	for (size_t i = 0; i < children.size(); ++i)
+	{
+		AstNode	*child = children[i];
+
+		if (!child)
+			continue;
+		if (child->type() == "location")
+			serv.locations.push_back(extractLocation(child));
+		else if (child->type() == "cgi")
+			_appendCgiItem(serv.d_cgiExts, child);
+		else if (child->type() == "error_page")
+			_appendErrorsPageItem(serv.d_errorPages, serv.d_defaultErrPage, child);
+	}
+}
+
+Config::Server	extractServer(const AstNode *serverNode)
+{
+	Config::Server	srv;
+
+	extractServDirectives(srv, serverNode);
+	std::sort(srv.locations.begin(), srv.locations.end());
+	return srv;
+}
+
+} // namespace config_parse
