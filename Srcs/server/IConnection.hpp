ebcf21e47d1e22c7cbd47eb7e1cc175dbcbd923c
um4ss <sliziard@icloud.com>
2026-01-09 10:44:24 +0100

    feat(server): introduce IConnection interface for poll-based connections

---
+++ Srcs/server/IConnection.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IConnection.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 12:13:50 by sliziard          #+#    #+#             */
+/*   Updated: 2026/01/08 17:41:01 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __ICONNECTION_HPP__
+# define __ICONNECTION_HPP__
+
+# include <sys/poll.h>
+
+class IConnection {
+
+private:
+
+public:
+	virtual ~IConnection() {};
+
+	virtual int				fd(void) const = 0;
+	virtual struct pollfd	pollFd(void) const = 0;
+
+	virtual short			events(void) const = 0;
+	virtual void			setEvents(short events) = 0;
+	virtual void			addEvent(short event) = 0;
+	virtual bool			handleEvents(short revents) = 0;
+};
+
+#endif /* __ICONNECTION_HPP__ */
