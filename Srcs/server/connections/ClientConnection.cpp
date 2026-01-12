97e17bbd3decb38e77d91ac4beb9482de6da4650
um4ss <sliziard@icloud.com>
2026-01-12 12:17:22 +0100

    test(server): add a minimal cli connection logic
    just print received buff then send an hello world http response

--- Srcs/server/connections/ClientConnection.cpp
+++ Srcs/server/connections/ClientConnection.cpp
@@ -6,10 +6,11 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 09:55:10 by sliziard          #+#    #+#             */
-/*   Updated: 2026/01/09 14:38:21 by sliziard         ###   ########.fr       */
+/*   Updated: 2026/01/09 21:08:43 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include <sys/poll.h>
 
 #include "ClientConnection.hpp"
@@ -48,7 +49,32 @@ bool	ClientConnection::processIO(short revents)
 		_request.reset();
 		_response.reset();
 	}
-*/  (void)revents;
+*/
+	if (revents & POLLIN)
+	{
+		char	buf[1024];
+		ssize_t	n = recv(_fd, buf, sizeof(buf) - 1, 0);
+
+		if (n > 0)
+		{
+			buf[n] = 0;
+			std::cout << "Received from fd " << _fd << ":\n" << buf << std::endl;
+			_events = POLLOUT;
+		}
+		else if (n <= 0)
+			return false;
+	}
+	if (revents & POLLOUT)
+	{
+		std::string	resp = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+		ssize_t		sent = send(_fd, resp.c_str(), resp.length(), 0);
+
+		if (sent < 0)
+			std::cerr << "failed to send response to fd " << _fd << std::endl;
+		else
+			std::cout << "Response sent to fd " << _fd << std::endl;
+		_events = POLLIN;
+	}
 	return true;
 }
 
