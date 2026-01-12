2c028aa825d6bc0f2a3a75e953221ec6bc09879f
um4ss <sliziard@icloud.com>
2026-01-12 17:47:37 +0100

    test: add a minimal config file and main to run server

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,50 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 18:07:04 by sliziard          #+#    #+#             */
-/*   Updated: 2025/12/08 18:07:04 by sliziard         ###   ########.fr       */
+/*   Updated: 2026/01/12 12:27:38 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include <cstddef>
+#include <exception>
+#include <iostream>
+
+#include "config/Config.hpp"
+#include "server/Reactor.hpp"
+#include "server/connections/ServerConnection.hpp"
+#include "ftpp/FtppException.hpp"
+
+#define ERR_USAGE	"Usage: webserv <configuration file>"
+
+int main(int ac, char **av)
 {
-	return 1;
+	if (ac != 2)
+		return (std::cerr << ERR_USAGE << std::endl, 2);
+
+	try {
+		Config	conf(av[1]);
+#ifdef WS_CONFIG_DEBUG
+		conf.print(std::cout);
+#endif
+
+		Reactor								pollManager;
+		const std::vector<Config::Server>	&servs = conf.getServers();
+
+		for (size_t i = 0; i < servs.size(); ++i)
+			pollManager.addConnection(new ServerConnection(servs[i]));
+
+		pollManager.run();
+	}
+	catch (const FtppException &parseErr)
+	{
+		std::cerr
+			<< "Failed to parse " << av[1]
+			<< ", check your configuration.\n"
+			<< parseErr.what() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception catch: " << e.what();
+	}
+	return 0;
 }
