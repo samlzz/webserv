9018a1df6757b3f70dae25f680f16a8052f0601f
Adrien <chuadrien2003@gmail.com>
2026-01-20 17:08:46 +0100

    feat(response): toString

--- src/http/response/HttpResponse.cpp
+++ src/http/response/HttpResponse.cpp
@@ -6,7 +6,7 @@
 /*   By: achu <achu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 23:32:00 by achu              #+#    #+#             */
-/*   Updated: 2026/01/20 02:48:24 by achu             ###   ########.fr       */
+/*   Updated: 2026/01/20 17:07:16 by achu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,10 @@
 
 HttpResponse::HttpResponse(const Config::Server& pServer)
 	: _server(pServer) {
+	reset();
 }
 
 HttpResponse::~HttpResponse(void) {
-	
 }
 
 // =========================================================================== //
@@ -155,14 +155,18 @@ static inline bool		isExtAuth(const std::string &pExt, const Config::t_dict &pEx
 //                            MEMBER FUNCTION                                  //
 // =========================================================================== //
 
-void		HttpResponse::build(const HttpRequest &pReq)
+ConnEvent		HttpResponse::build(const HttpRequest &pReq, IWritableNotifier &notifier)
 {
 	_request = pReq;
+	// TODO: Check request error
+
 	std::string	path = _request.getPath();
 
 	_location =	_server.findLocation(subLocaPath(path));
-	if (!_location)
-		return (handleERROR(http::SC_NOT_FOUND));
+	if (!_location) {
+		setError(http::SC_NOT_FOUND);
+		return ConnEvent::none();
+	}
 
 	if (_location->redirect) {
 		addHeader("Location", _location->redirect.get()->path);
@@ -174,27 +178,32 @@ void		HttpResponse::build(const HttpRequest &pReq)
 	std::vector<http::e_method>::const_iterator	itMethods;
 	itMethods = std::find(_location->methods.begin(), _location->methods.end(), curMethod);
 	if (itMethods == _location->methods.end()) {
-		//TODO: handle error 405 method not allowed
 		addHeader("Allow", methodsTOstring(_location->methods));
-		return ;
+		setError(http::SC_METHOD_NOT_ALLOWED);
+		return ConnEvent::none();
 	}
 
 	bool		isCgi = false;
 	std::string	interpreterPath;
 
-	for (Config::t_dict::const_iterator itCgiExts = _location->cgiExts.begin(); itCgiExts !=_location->cgiExts.end(); itCgiExts++) {
-		if (subExt(path) == itCgiExts->first) {
-			interpreterPath = itCgiExts->second;
-			isCgi = true;
-			break;	
-		}
+	Config::t_dict::const_iterator	it = _location->cgiExts.find(subExt(path));
+	if (it != _location->cgiExts.end()) {
+		interpreterPath = it->second;
+		isCgi = true;
 	}
 
 	if (isCgi) {
-		if (access((_location->path + path).c_str(), F_OK) != 0) return ; //TODO: handle error 404 not found
-		if (access((_location->path + path).c_str(), R_OK) != 0) return ; //TODO: handle error 403 forbidden
+		if (access((_location->path + path).c_str(), F_OK) != 0) {
+			setError(http::SC_NOT_FOUND);
+			return ConnEvent::none();
+		}
+
+		if (access((_location->path + path).c_str(), R_OK) != 0) {
+			setError(http::SC_FORBIDDEN);
+			return ConnEvent::none();
+		}
 		//TODO: CGI stuff
-		return ;
+		return ConnEvent::none();
 	}
 
 	switch (_request.getMethod()) {
@@ -203,42 +212,30 @@ void		HttpResponse::build(const HttpRequest &pReq)
 	case http::MTH_POST:	handlePOST(); break;
 	case http::MTH_PUT:		handlePUT(); break;
 	case http::MTH_DELETE:	handleDELETE(); break;
-	default: return; //TODO: handle error 501 not implemented
+	default:
+		setError(http::SC_NOT_IMPLEMENTED);
+		return ConnEvent::none();
 	}
 
 	//TODO: toString
 }
 
-bool		HttpResponse::isDone(void) const
-{
-	
-}
+bool		HttpResponse::isDone(void) const { return (_isDone);}
 
 // Clean all the variable in the response class to prepare for the next incoming request
 void		HttpResponse::reset(void)
 {
 	_location = NULL;
+
 	_response.statusCode.code = 0;
 	_response.statusCode.reason.clear();
 	_response.headers.clear();
 	_response.body.clear();
+
+	_isDone = false;
 }
 
 bool		HttpResponse::isConnectionClose(void) const
-{
-	
-}
-
-// ---------------------------------------------------------------------
-// Output production (transport-driven)
-// ---------------------------------------------------------------------
-
-bool		HttpResponse::produceNext(void)
-{
-	
-}
-
-const std::string&		HttpResponse::buffer(void) const
 {
 	
 }
@@ -256,16 +253,8 @@ void HttpResponse::addHeader(const std::string &pHeader, const std::string &pCon
 	_response.headers[pHeader] = pContent;
 }
 
-// ------------------------------------------
-
 void	HttpResponse::loadFile(const std::string& pPath)
 {
-	std::ifstream file(pPath.c_str(), std::ios::binary);
-	if (!file) {
-		handleERROR(http::SC_FORBIDDEN);
-		return;
-	}
-
 	struct stat	st;
 	stat(pPath.c_str(), &st);
 	std::string	ext = subExt(pPath);
@@ -280,9 +269,11 @@ void		HttpResponse::handleGET(void)
 	std::string	path = _location->path + _request.getPath();
 
 	if (stat(path.c_str(), &st) != 0)
-		return ; //TODO: handle 404
+		return setError(http::SC_NOT_FOUND);
 
 	if (S_ISDIR(st.st_mode)) {
+		//TODO: access folder
+
 		if (path[path.length() - 1] != '/') {
 			std::string redirectPath = _request.getPath() + "/";
 			addHeader("Location", redirectPath);
@@ -299,7 +290,7 @@ void		HttpResponse::handleGET(void)
 		{
 			DIR *dir = opendir(path.c_str());
 			if (!dir)
-				return; // TODO: handle 403 forbidden
+				return setError(http::SC_FORBIDDEN);
 	
 			struct dirent				*entry;
 			std::vector<std::string>	folders;
@@ -325,88 +316,25 @@ void		HttpResponse::handleGET(void)
 			closedir(dir);
 			return;
 		}
-
-		handleERROR(http::SC_FORBIDDEN);
-		return ;
 	}
 
-	if (isRegFile(path)) {
+	if (S_ISREG(st.st_mode)) {
 		loadFile(path);
 		return ;
 	}
 
-	handleERROR(http::SC_NOT_FOUND);
+	return setError(http::SC_NOT_FOUND);
 }
 
 void		HttpResponse::handleHEAD(void)
 {
-	struct stat	st;
-	std::string	path = _location->path + _request.getPath();
 
-	if (stat(path.c_str(), &st) != 0)
-		return ; //TODO: handle 404
-
-	if (S_ISDIR(st.st_mode)) {
-		if (path[path.length() - 1] != '/') {
-			std::string redirectPath = _request.getPath() + "/";
-			addHeader("Location", redirectPath);
-			return ; //TODO: handle 301 MOVED PERMA
-		}
-
-		std::string fullIndex = path + _location->index;
-		if (isRegFile(fullIndex)) {
-			loadFile(fullIndex);
-			return ;
-		}
-
-		if (_location->autoindex)
-		{
-			DIR *dir = opendir(path.c_str());
-			if (!dir)
-				return; // TODO: handle 403 forbidden
-	
-			struct dirent				*entry;
-			std::vector<std::string>	folders;
-			std::vector<std::string>	files;
-
-			while ((entry = readdir(dir)) != NULL) {
-				struct stat s;
-				if (stat(entry->d_name, &s) == 0) {
-					if (S_ISDIR(s.st_mode))
-						folders.push_back(entry->d_name);
-					if (S_ISREG(s.st_mode))
-						files.push_back(entry->d_name);
-				}
-			}
-			std::sort(folders.begin(), folders.end());
-			std::sort(files.begin(), files.end());
-
-			for (size_t i = 0; i < folders.size(); ++i)
-				_response.body += "<a href=\"" + folders[i] + "\">" + folders[i] + "</a><br>\n";
-			for (size_t i = 0; i < files.size(); ++i)
-				_response.body += "<a href=\"" + files[i] + "\">" + files[i] + "</a><br>\n";
-
-			closedir(dir);
-			return;
-		}
-
-		handleERROR(http::SC_FORBIDDEN);
-		return ;
-	}
-
-	if (isRegFile(path)) {
-		loadFile(path);
-		return ;
-	}
-
-	handleERROR(http::SC_NOT_FOUND);
 }
 
 // TODO: need a Reverse Deque Technique like send but receive instead, might need to redo request 
 void		HttpResponse::handlePOST(void)
 {
-	
-	return ; // thorw 405 method not allowed
+
 }
 
 void		HttpResponse::handlePUT(void)
@@ -417,27 +345,27 @@ void		HttpResponse::handlePUT(void)
 	bool	fileExist = (stat(path.c_str(), &st) == 0);
 
 	if (fileExist && isDirectory(path))
-		return handleERROR(http::SC_FORBIDDEN);
+		return setError(http::SC_FORBIDDEN);
 
 	if (fileExist && access(path.c_str(), W_OK) != 0)
-		return handleERROR(http::SC_FORBIDDEN);
+		return setError(http::SC_FORBIDDEN);
 
 	if (!fileExist) {
 		std::string parent = path.substr(0, path.find_last_of('/'));
 		if (stat(parent.c_str(), &st) != 0 || access(parent.c_str(), W_OK) != 0)
-			return handleERROR(http::SC_FORBIDDEN);
+			return setError(http::SC_FORBIDDEN);
 	}
 
 	int		fd = open(path.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
-		return handleERROR(http::SC_INTERNAL_SERVER_ERROR);
+		return setError(http::SC_INTERNAL_SERVER_ERROR);
 
 	std::string body = _request.getBody();
 	size_t	written = write(fd, body.c_str(), body.length());
 	close(fd);
 
 	if (written < 0)
-		return handleERROR(http::SC_INTERNAL_SERVER_ERROR);
+		return setError(http::SC_INTERNAL_SERVER_ERROR);
 
 	fileExist ? _response.setStatusCode(http::SC_NO_CONTENT) : _response.setStatusCode(http::SC_CREATED);
 }
@@ -449,15 +377,54 @@ void	HttpResponse::handleDELETE(void)
 	struct stat st;
 
 	if (stat(path.c_str(), &st) != 0)
-		return ; //TODO: handle 404
+		return setError(http::SC_NOT_FOUND);
 
 	if (S_ISDIR(st.st_mode))
-		return ; //TODO: handle 403
+		return setError(http::SC_FORBIDDEN);
 
 	if (unlink(path.c_str()) != 0)
-		return ; //TODO: handle 500
+		return setError(http::SC_INTERNAL_SERVER_ERROR);
 
 	_response.setStatusCode(http::SC_NO_CONTENT);
+}
+
+void	HttpResponse::setError(int pCode)
+{
+	_response.body.clear();
+	_response.statusCode.code = pCode;
+	_response.statusCode.reason = HttpData::getStatusType(pCode);
+
+	std::ostringstream	stream;
+	stream	<< "<html>\r\n"
+			<< "<head><title>" << pCode << " " << HttpData::getStatusType(pCode) << "</title></head>\r\n"
+			<< "<body>\r\n"
+			<< "<h1>" << pCode << " " << HttpData::getStatusType(pCode) << "</h1>\r\n"
+			<< "<p> An error has occured. </p>\r\n"
+			<< "</body>\r\n"
+			<< "</html>\r\n";
+	_response.body = stream.str();
+
+	addHeader("Content-Type", HttpData::getMimeType("text/html"));
+	addHeader("Content-Length", longTOstring(_response.body.length()));
+}
+
+std::string	HttpResponse::toString(void) const
+{
+	std::ostringstream	stream;
+
+	stream	<< "HTTP/1.1 " << _response.statusCode.code << " " << _response.statusCode.reason << "\r\n";
+
+	t_headers	headers = _response.headers;
+	t_headers::const_iterator	it;
+	for (it = headers.begin(); it != headers.end(); it++) {
+		stream << it->first << ": " << it->second << "\r\n";
+	}
+	stream << "\r\n";
+
+	if (!_response.body.empty()) {
+		stream << _response.body;
+		
+	}
 }
 
 //#****************************************************************************#
--- src/http/response/HttpResponse.hpp
+++ src/http/response/HttpResponse.hpp
@@ -16,8 +16,11 @@
 #include <string>
 
 #include "http/response/IHttpResponse.hpp"
+#include "http/response/ChunkedStream.hpp"
 #include "http/request/HttpRequest.hpp"
 #include "http/HttpData.hpp"
+
+#include "server/connections/ConnEvent.hpp"
 #include "config/Config.hpp"
 
 class HttpResponse : public IHttpResponse {
@@ -26,15 +29,14 @@ public:
 	HttpResponse(const Config::Server& pServer);
 	~HttpResponse(void);
 
-	// ========== State Control ==========
-	virtual void	build(const HttpRequest& pRequest);
-	virtual bool	isDone() const;
-	virtual void	reset(void);
-	virtual bool	isConnectionClose(void) const;
+	// ========== Life Cycle ==========
+	virtual ConnEvent	build(const HttpRequest& pRequest, IWritableNotifier &notifier);
+	virtual void		reset(void);
 
 	// ========== Output Production ==========
-	virtual bool					produceNext(void);
-	virtual const std::string&		buffer() const;
+	virtual IChunkedStream&		stream(void);
+	virtual bool				isDone() const;
+	virtual bool				isConnectionClose(void) const;
 
 private:
 
@@ -60,16 +62,22 @@ private:
 	const Config::Server				&_server;
 	const Config::Server::Location		*_location;
 
-	Response							_response;
+	Response		_response;
+
+	bool				_isDone;
+	ChunkedStream		_chunkedStream;
+
+	void		setError(int pCode);
 
 	void		handleGET(void);
 	void		handleHEAD(void);
 	void		handlePOST(void);
 	void		handlePUT(void);
 	void		handleDELETE(void);
-	void		handleERROR(int pCode);
 
 	void		loadFile(const std::string& pPath);
+
+	std::string		toString(void) const;
 };
 
 #endif
