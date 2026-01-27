eef425ad1ba9d529b805f04d05d53c3f90cbef9f
um4ss <sliziard@icloud.com>
2026-01-27 18:30:41 +0100

    feat(cgi): add CGI output parser for headers and body

---
+++ src/http/cgi/CgiOutputParser.cpp
@@ -0,0 +1,181 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CgiOutputParser.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 15:25:30 by sliziard          #+#    #+#             */
+/*   Updated: 2026/01/27 15:37:12 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "CgiOutputParser.hpp"
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+
+// ============================================================================
+// Construction / Destruction
+// ============================================================================
+
+CgiOutputParser::CgiOutputParser()
+	: _state(ST_HEADERS)
+	, _headBuf()
+	, _bodyQ()
+	, _status(http::SC_OK)
+	, _headers()
+{}
+
+CgiOutputParser::~CgiOutputParser()
+{}
+
+// ============================================================================
+// IOutputSink
+// ============================================================================
+
+void CgiOutputParser::append(const char* buf, size_t n)
+{
+	if (_state == ST_DONE || !buf || !n)
+		return;
+
+	if (_state == ST_HEADERS)
+	{
+		_headBuf.append(buf, n);
+		if (tryParseHeaders())
+			return;
+	}
+	else if (_state == ST_BODY)
+		_bodyQ.push_back(std::string(buf, n));
+}
+
+void CgiOutputParser::finalize(void)
+{
+	_state = ST_DONE;
+}
+
+// ============================================================================
+// IMetaSource
+// ============================================================================
+
+bool CgiOutputParser::metaReady() const
+{
+	return _state != ST_HEADERS;
+}
+
+http::e_status_code CgiOutputParser::status() const
+{
+	return _status;
+}
+
+const http::t_headers& CgiOutputParser::headers() const
+{
+	return _headers;
+}
+
+// ============================================================================
+// Body access
+// ============================================================================
+
+bool CgiOutputParser::bodyHasData() const
+{
+	return !_bodyQ.empty();
+}
+
+size_t CgiOutputParser::bodyRead(char* dst, size_t max)
+{
+	if (!dst || max == 0 || _bodyQ.empty())
+		return 0;
+
+	std::string& front = _bodyQ.front();
+	size_t n = front.size();
+	if (n > max)
+		n = max;
+
+	std::memcpy(dst, front.data(), n);
+
+	if (n == front.size())
+		_bodyQ.pop_front();
+	else
+		front.erase(0, n);
+
+	return n;
+}
+
+bool CgiOutputParser::eof() const
+{
+	return _state == ST_DONE && _bodyQ.empty();
+}
+
+// ============================================================================
+// Header parsing
+// ============================================================================
+
+bool CgiOutputParser::tryParseHeaders()
+{
+	size_t pos = std::string::npos;
+	size_t sepLen = 0;
+
+	if ((pos = _headBuf.find("\r\n\r\n")) != std::string::npos)
+		sepLen = 4;
+	else if ((pos = _headBuf.find("\n\n")) != std::string::npos) // ? fallback to \n\n
+		sepLen = 2;
+	else
+		return false;
+
+	std::string headerBlock = _headBuf.substr(0, pos);
+	parseHeaderLines(headerBlock);
+
+	_state = ST_BODY;
+
+	size_t bodyStart = pos + sepLen;
+	if (bodyStart < _headBuf.size())
+		_bodyQ.push_back(_headBuf.substr(bodyStart));
+	_headBuf.clear();
+	return true;
+}
+
+static std::string _trim(const std::string& s)
+{
+	size_t start = 0;
+	while (start < s.size() && (s[start] == ' ' || s[start] == '\t'))
+		start++;
+
+	size_t end = s.size();
+	while (end > start && (s[end - 1] == ' ' || s[end - 1] == '\t'))
+		end--;
+
+	return s.substr(start, end - start);
+}
+
+void CgiOutputParser::parseHeaderLines(const std::string& headerBlock)
+{
+	std::istringstream iss(headerBlock);
+	std::string line;
+
+	while (std::getline(iss, line))
+	{
+		if (!line.empty() && line[line.size() - 1] == '\r')
+			line.erase(line.size() - 1);
+
+		if (line.empty())
+			continue;
+
+		size_t colon = line.find(':');
+		if (colon == std::string::npos)
+			continue;
+
+		std::string key = _trim(line.substr(0, colon));
+		std::string val = _trim(line.substr(colon + 1));
+
+		if (key == "Status")
+		{
+			int code = std::atoi(val.c_str());
+			if (code >= 100 && code <= 599)
+				_status = static_cast<http::e_status_code>(code);
+		}
+		else
+		_headers[key] = val;
+	}
+}
+
---
+++ src/http/cgi/CgiOutputParser.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CgiOutputParser.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 14:50:30 by sliziard          #+#    #+#             */
+/*   Updated: 2026/01/27 15:36:54 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __CGI_OUTPUT_PARSER_HPP__
+# define __CGI_OUTPUT_PARSER_HPP__
+
+# include <string>
+# include <deque>
+
+# include "IOutputSink.hpp"
+# include "http/HttpTypes.hpp"
+# include "http/response/interfaces/IMetaSource.hpp"
+
+class CgiOutputParser : public IMetaSource, public IOutputSink {
+
+private:
+	enum e_state { ST_HEADERS, ST_BODY, ST_DONE };
+	e_state					_state;
+
+	std::string				_headBuf;
+	std::deque<std::string>	_bodyQ;
+
+	http::e_status_code		_status;
+	http::t_headers			_headers;
+
+public:
+	CgiOutputParser();
+	virtual ~CgiOutputParser();
+
+	// IOutputSink
+	virtual void					append(const char* buf, size_t n);
+	virtual void					finalize(void);
+
+	// IMetaSource
+	virtual bool					metaReady() const;
+	virtual http::e_status_code		status() const;
+	virtual const http::t_headers&	headers() const;
+
+	// body access
+	bool							bodyHasData() const;
+	size_t							bodyRead(char* dst, size_t max);
+	bool							eof() const; // EOF CGI atteint
+
+private:
+
+	bool tryParseHeaders(); // ? returns true when headers completed+parsed
+	void parseHeaderLines(const std::string& headerBlock);
+
+	// forbidden
+	CgiOutputParser(const CgiOutputParser &other);
+	CgiOutputParser	&operator=(const CgiOutputParser &outher);
+};
+
+#endif
