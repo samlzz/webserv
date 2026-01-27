51bee20c22ed4c3e78403dffecc87f98428d50bb
um4ss <sliziard@icloud.com>
2026-01-27 18:32:37 +0100

    feat(http): add CGI-backed BodySource with meta support

---
+++ src/http/handlers/bodySrcs/CgiBodySource.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CgiBodySource.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 16:07:39 by sliziard          #+#    #+#             */
+/*   Updated: 2026/01/27 18:04:03 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "CgiBodySource.hpp"
+#include "http/cgi/CgiOutputParser.hpp"
+#include "http/cgi/CgiProcess.hpp"
+
+// ============================================================================
+// Construction / Destruction
+// ============================================================================
+
+CgiBodySource::CgiBodySource(CgiProcess *proc, CgiOutputParser *parser)
+	: _parser(parser), _process(proc)
+{}
+
+CgiBodySource::~CgiBodySource()
+{}
+
+// ============================================================================
+// Methods
+// ============================================================================
+
+// ---- IBodySource ----
+
+bool CgiBodySource::hasMore(void) const
+{
+	if (_parser->bodyHasData())
+		return true;
+
+	if (!_process->isTerminated())
+		return true;
+
+	return false;
+}
+
+bool CgiBodySource::hasError(void) const
+{
+	return _process->isError();
+}
+
+bool CgiBodySource::terminated() const
+{
+	return _process->isTerminated() && !_parser->bodyHasData();
+}
+
+size_t CgiBodySource::read(char* dst, size_t max)
+{
+	if (!dst || max == 0)
+		return 0;
+
+	if (_parser->bodyHasData())
+		return _parser->bodyRead(dst, max);
+
+	return 0;
+}
+
+// ---- IMetaSource ----
+
+bool CgiBodySource::metaReady() const
+{
+	return _parser->metaReady();
+}
+
+http::e_status_code CgiBodySource::status() const
+{
+	return _parser->status();
+}
+
+const http::t_headers& CgiBodySource::headers() const
+{
+	return _parser->headers();
+}
---
+++ src/http/handlers/bodySrcs/CgiBodySource.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CgiBodySource.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/27 15:06:00 by sliziard          #+#    #+#             */
+/*   Updated: 2026/01/27 18:03:24 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __CGI_BODYSOURCE_HPP__
+# define __CGI_BODYSOURCE_HPP__
+
+# include <cstddef>
+# include <unistd.h>
+
+# include "http/cgi/CgiOutputParser.hpp"
+# include "http/cgi/CgiProcess.hpp"
+# include "http/response/interfaces/IBodySource.hpp"
+# include "http/response/interfaces/IMetaSource.hpp"
+
+class CgiBodySource : public IBodySource, public IMetaSource {
+private:
+	CgiOutputParser	*_parser; // owned
+	CgiProcess		*_process; // observed
+
+public:
+	CgiBodySource(CgiProcess *proc, CgiOutputParser *parser);
+	virtual ~CgiBodySource();
+
+	// IBodySource
+	virtual bool					hasMore(void) const;
+	virtual bool					hasError(void) const;
+	virtual bool					terminated(void) const;
+
+	virtual size_t					read(char* dst, size_t max);
+
+	// IMetaSource
+	virtual bool					metaReady() const;
+	virtual http::e_status_code		status() const;
+	virtual const http::t_headers&	headers() const;
+
+private:
+	// forbidden#
+	CgiBodySource(const CgiBodySource &other);
+	CgiBodySource& operator=(const CgiBodySource &other);
+};
+
+
+#endif /* __CGI_BODYSOURCE_HPP__ */
