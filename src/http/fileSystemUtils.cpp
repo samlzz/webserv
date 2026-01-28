0bc69183573b382463ed33d3e333ac6bd8f488cb
um4ss <sliziard@icloud.com>
2026-01-28 14:48:01 +0100

    feat(http): centralize filesystem access into fs utilities

---
+++ src/http/fileSystemUtils.cpp
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fileSystemUtils.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 14:41:48 by sliziard          #+#    #+#             */
+/*   Updated: 2026/01/28 14:42:29 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+#include <stdint.h>
+
+#include "fileSystemUtils.hpp"
+
+namespace fs
+{
+
+// -----------------------------------------------------------------------------
+// checkPerms
+// -----------------------------------------------------------------------------
+bool checkPerms(const std::string &path, int32_t perms)
+{
+	struct stat st;
+
+	if (stat(path.c_str(), &st) != 0)
+		return false;
+
+	if ((perms & P_EXIST) && errno == ENOENT)
+		return false;
+
+	if (perms & P_READ)
+	{
+		if (!(st.st_mode & S_IRUSR))
+			return false;
+	}
+
+	if (perms & P_WRITE)
+	{
+		if (!(st.st_mode & S_IWUSR))
+			return false;
+	}
+
+	if (perms & P_EXEC)
+	{
+		if (!(st.st_mode & S_IXUSR))
+			return false;
+	}
+
+	return true;
+}
+
+// -----------------------------------------------------------------------------
+// isFile
+// -----------------------------------------------------------------------------
+bool isFile(const std::string &path)
+{
+	struct stat st;
+	if (stat(path.c_str(), &st) != 0)
+		return false;
+	return S_ISREG(st.st_mode);
+}
+
+// -----------------------------------------------------------------------------
+// isDir
+// -----------------------------------------------------------------------------
+bool isDir(const std::string &path)
+{
+	struct stat st;
+	if (stat(path.c_str(), &st) != 0)
+		return false;
+	return S_ISDIR(st.st_mode);
+}
+
+// -----------------------------------------------------------------------------
+// openReadOnly
+// -----------------------------------------------------------------------------
+int openReadOnly(const std::string &path)
+{
+	int fd = open(path.c_str(), O_RDONLY | O_CLOEXEC);
+	return fd;
+}
+
+// -----------------------------------------------------------------------------
+// subExt
+// -----------------------------------------------------------------------------
+std::string subExt(const std::string &filePath)
+{
+	size_t slash = filePath.find_last_of('/');
+	size_t dot   = filePath.find_last_of('.');
+
+	if (dot == std::string::npos)
+		return "";
+
+	if (slash != std::string::npos && dot < slash)
+		return "";
+
+	if (dot + 1 >= filePath.size())
+		return "";
+
+	return filePath.substr(dot + 1);
+}
+
+// -----------------------------------------------------------------------------
+// size
+// -----------------------------------------------------------------------------
+ssize_t size(const std::string &path)
+{
+	struct stat st;
+	if (stat(path.c_str(), &st) != 0)
+		return -1;
+
+	return static_cast<ssize_t>(st.st_size);
+}
+
+} // namespace fs
+
