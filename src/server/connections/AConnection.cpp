5eaa6aafef9c9d1d1ff089a02199dd16902821f5
um4ss <sliziard@icloud.com>
2026-02-02 07:56:57 +0100

    fix(server): correctly set FD flags and restore FD_CLOEXEC handling

--- src/server/connections/AConnection.cpp
+++ src/server/connections/AConnection.cpp
@@ -63,9 +63,17 @@ void	AConnection::addEvent(short event)		{ _events |= event; }
 
 void	AConnection::setFdFlags(void)
 {
-	int32_t flags = fcntl(_fd, F_GETFL, 0);
+	int32_t flags;
+	
+	flags = fcntl(_fd, F_GETFL);
 	if (flags == -1
-		|| fcntl(_fd, F_SETFL, flags | O_NONBLOCK | O_CLOEXEC) == -1
+		|| fcntl(_fd, F_SETFL, flags | O_NONBLOCK) == -1
+	)
+		throw SysError("fcntl");
+
+	flags = fcntl(_fd, F_GETFD);
+	if (flags == -1
+		|| fcntl(_fd, F_SETFD, flags | FD_CLOEXEC) == -1
 	)
 		throw SysError("fcntl");
 }
