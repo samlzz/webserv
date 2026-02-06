f6967e5e0d95d7e7dcb3738e9b70e8866023c8f6
um4ss <sliziard@icloud.com>
2026-02-06 13:02:48 +0100

    fix(cgi): update exit code and improve error checking in CgiBodySource

--- src/http/handlers/bodySrcs/CgiBodySource.cpp
+++ src/http/handlers/bodySrcs/CgiBodySource.cpp
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 16:07:39 by sliziard          #+#    #+#             */
-/*   Updated: 2026/02/02 13:22:43 by sliziard         ###   ########.fr       */
+/*   Updated: 2026/02/06 12:03:59 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,9 @@ bool CgiBodySource::hasMore(void) const
 
 bool CgiBodySource::hasError(void) const
 {
-	return _process->isError();
+	if (_process->isError())
+		return true;
+	return _process->exitCode() != 0;
 }
 
 bool CgiBodySource::terminated() const
@@ -79,7 +81,7 @@ bool CgiBodySource::metaReady() const
 
 http::e_status_code CgiBodySource::status() const
 {
-	if (_process->isError())
+	if (hasError())
 	{
 		if (_process->isTimeout())
 			return http::SC_GATEWAY_TIMEOUT;
