91eef4072dbfdba2b0b75641e5150aaad463991e
um4ss <sliziard@icloud.com>
2026-02-20 16:16:21 +0100

    feat(HttpTransaction): add HttpTransaction class to handle transactions
    A transaction is the switch from request to response (ResponsePlan in
    that case)
    It include the routing, the verifiaction and the dispatch

---
+++ src/http/transaction/HttpTransaction.hpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HttpTransaction.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/20 11:24:59 by sliziard          #+#    #+#             */
+/*   Updated: 2026/02/20 13:00:20 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __HTTP_TRANSACTION_HPP__
+# define __HTTP_TRANSACTION_HPP__
+
+# include "http/request/Cookies.hpp"
+# include "http/request/HttpRequest.hpp"
+# include "http/response/ResponsePlan.hpp"
+# include "http/routing/Router.hpp"
+# include "server/AddrInfo.hpp"
+# include "server/ServerCtx.hpp"
+# include "utils/Optionnal.hpp"
+
+class HttpTransaction {
+
+private:
+	Optionnal<routing::Context>	_route;
+	ServerCtx					&_ctx;
+	const AddrInfo				_remote;
+	const AddrInfo				_local;
+
+public:
+
+	// ========================================================================
+	// Construction / Destruction
+	// ========================================================================
+	HttpTransaction(ServerCtx &serv,
+					const AddrInfo &remote,
+					const AddrInfo &local
+				);
+	~HttpTransaction();
+
+	// ========================================================================
+	// Methods
+	// ========================================================================
+
+	ResponsePlan			onParsingError(const HttpRequest &req);
+	Optionnal<ResponsePlan>	onHeadersComplete(const HttpRequest &req);
+	ResponsePlan			onBodyComplete(const HttpRequest &req);
+
+	bool					isHeadersValidated(void) const;
+	bool					shouldCloseConnection(void) const;
+
+private:
+	void					handleSession(Cookies &store);
+	void					setCookies(Cookies &store,
+										const std::string &query) const;
+
+	// ? Forbidden
+	HttpTransaction();
+	HttpTransaction(const HttpTransaction &other);
+	HttpTransaction& operator=(const HttpTransaction &other);
+};
+
+
+#endif /* __HTTP_TRANSACTION_HPP__ */
