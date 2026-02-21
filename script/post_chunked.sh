466624a6cc2344ca8f6919351bbd56ecfd8d5913
Kevin WANG <sirewk@hotmail.fr>
2026-02-21 16:06:28 +0100

    fix(script): correct chunk sizes and formatting in post_chunked.sh

--- script/post_chunked.sh
+++ script/post_chunked.sh
@@ -5,15 +5,15 @@ Host: localhost:$PORT\r
 Content-Type: application/octet-stream\r
 Transfer-Encoding: chunked\r
 \r
-4\r
-Ceci\r
+5\r
+Ceci \r
 7\r
- est un\r
+est un \r
 8\r
- test de\r
+test de \r
 e\r
- message envoyé\r
-d\r
+message envoye\r
+e\r
  par morceaux.\r
 0\r
 \r
